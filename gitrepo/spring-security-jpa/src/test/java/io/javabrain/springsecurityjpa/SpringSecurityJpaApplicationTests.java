30bd7810a53488ef40d6302e5f28dd4de7be92d4
ritesh9840 <52201727+ritesh9840@users.noreply.github.com>
2020-06-26 11:47:35 +0530

    Update SpringSecurityJpaApplicationTests.java

--- gitrepo/spring-security-jpa/src/test/java/io/javabrain/springsecurityjpa/SpringSecurityJpaApplicationTests.java
+++ gitrepo/spring-security-jpa/src/test/java/io/javabrain/springsecurityjpa/SpringSecurityJpaApplicationTests.java
@@ -2,7 +2,7 @@ package io.javabrain.springsecurityjpa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//adding comment
 @SpringBootTest
 class SpringSecurityJpaApplicationTests {
 
